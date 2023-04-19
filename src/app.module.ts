d7f58f760cb0143a485884fde9e60b16ea8805af
vinicius <viniciusguerraf@gmail.com>
2023-04-19 20:58:29 -0300

    feat: add mongo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongoModule } from './mongo/mongo.module';
 
 @Module({
-  imports: [],
+  imports: [MongoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
